ce4d42f861ea18db907565e119a388ce14ef773e
ismail alkouz <ismailkouz2015@gmail.com>
2021-01-08 21:57:52 +0200

    enhancements

--- src/main/java/com/example/its/util/Constants.java
+++ src/main/java/com/example/its/util/Constants.java
@@ -2,4 +2,6 @@ package com.example.its.util;
 
 public class Constants {
     public static final String CURRENT_USER = "currentUser";
+    public static final String EDIT="edit";
+    public static final String VIEW="view";
 }
